3ea08f2e0840b60619d412ac485dd17817838630
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-04 15:48:49 +0200

    Fix register.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,21 +3,21 @@
         @csrf
 
         <!-- First Name -->
-        <div>
-            <x-input-label for="firts_name" :value="__('First Name')" />
-            <x-text-input id="firts_name" class="block mt-1 w-full" type="text" name="firts_name" :value="old('firts_name')" required autofocus autocomplete="firts_name" />
-            <x-input-error :messages="$errors->get('firts_name')" class="mt-2" />
+        <div class="mt-4">
+            <x-input-label for="first_name" :value="__('First Name')" />
+            <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus autocomplete="first_name" />
+            <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
         </div>
 
         <!-- Last Name -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="last_name" :value="__('Last Name')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="last_name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
         </div>
 
         <!-- Departments -->
-        <div>
+        <div class="mt-4">
             <x-input-label for="departments" :value="__('Departments')" />
             <x-text-input id="departments" class="block mt-1 w-full" type="text" name="departments" :value="old('departments')" required autofocus autocomplete="departments" />
             <x-input-error :messages="$errors->get('departments')" class="mt-2" />
