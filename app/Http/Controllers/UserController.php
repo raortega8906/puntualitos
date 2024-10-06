76886864aaefbd943bfa6052efa9d7610a58234c
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-06 23:59:51 +0200

    user's CRUD with errors

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        dd($request->all()); die();
+
+//        User::create($request->validated());
+//
+//        return redirect()->route('users.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        dd($request->all());
+
+//        $user->update($request->validated());
+//
+//        return redirect()->route('users.index', compact('user'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        dd($user); die();
+
+//        $user->delete();
+    }
+}
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css"
           integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous">
     <!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="../../dist/css/adminlte.css"><!--end::Required Plugin(AdminLTE)--><!-- apexcharts -->
+    <link rel="stylesheet" href="../../../dist/css/adminlte.css"><!--end::Required Plugin(AdminLTE)--><!-- apexcharts -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css"
           integrity="sha256-4MX+61mt9NVvvuPjUWdUdyfZfxSB1/Rf9WtqRHgG5S0=" crossorigin="anonymous"><!-- jsvectormap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css"
@@ -49,15 +49,15 @@
                     </a></li> <!--end::Fullscreen Toggle--> <!--begin::User Menu Dropdown-->
                 <li class="nav-item dropdown user-menu"><a href="#" class="nav-link dropdown-toggle"
                                                            data-bs-toggle="dropdown"> <img
-                            src="../../dist/assets/img/avatar4.png" class="user-image rounded-circle shadow"
+                            src="../../../dist/assets/img/avatar4.png" class="user-image rounded-circle shadow"
                             alt="Avatar Image"> <span class="d-none d-md-inline">{{ Auth::user()->first_name }}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
-                        <li class="user-header text-bg-primary"><img src="../../dist/assets/img/avatar4.png"
+                        <li class="user-header text-bg-primary"><img src="../../../dist/assets/img/avatar4.png"
                                                                      class="rounded-circle shadow" alt="Avatar Image">
                             <p>
                                 {{ Auth::user()->first_name }}
-{{--                                <small>{{ __('Desde el 2024') }}</small>--}}
+                                {{--                                <small>{{ __('Desde el 2024') }}</small>--}}
                             </p>
                         </li> <!--end::User Image--> <!--begin::Menu Body-->
 
@@ -78,7 +78,7 @@
     </nav> <!--end::Header--> <!--begin::Sidebar-->
     <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
         <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="{{ route('dashboard')}}" class="brand-link">
-                <!--begin::Brand Image--> <img src="../../images/logo_bg_removed.png" alt="Puntualitos Logo"
+                <!--begin::Brand Image--> <img src="../../../images/logo_bg_removed.png" alt="Puntualitos Logo"
                                                class="brand-image opacity-75 shadow"> <!--end::Brand Image-->
                 <!--begin::Brand Text--> <span class="brand-text fw-light">{{ __('Puntualitos') }}</span> <!--end::Brand Text-->
             </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
@@ -150,13 +150,13 @@
             <div class="container-fluid"> <!--begin::Row-->
                 <div class="row">
                     <div class="col-sm-6">
-                        <h3 class="mb-0">{{ __('Dashboard') }}</h3>
+                        <h3 class="mb-0">{{ __('Crear Usuario') }}</h3>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-end">
                             <li class="breadcrumb-item"><a href="#">{{ __('Home') }}</a></li>
                             <li class="breadcrumb-item active" aria-current="page">
-                                {{ __('Dashboard') }}
+                                {{ __('Crear Usuario') }}
                             </li>
                         </ol>
                     </div>
@@ -165,74 +165,34 @@
         </div> <!--end::App Content Header--> <!--begin::App Content-->
         <div class="app-content"> <!--begin::Container-->
             <div class="container-fluid"> <!--begin::Row-->
-                <div class="row"> <!--begin::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 1-->
-                        <div class="small-box text-bg-primary">
-                            <div class="inner">
-                                <h3>150</h3>
-                                <p>New Orders</p>
+                <div class="card card-primary mb-4">
+                    <form method="POST" action="{{ route('users.update', $user) }}">
+                        @method('PUT')
+                        @csrf
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="first_name" class="form-label">{{ __('Nombre') }}</label>
+                                <input type="text" name="first_name" class="form-control" id="first_name" aria-describedby="first_name" value="{{ $user->first_name }}">
                             </div>
-                            <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                 xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path>
-                            </svg>
-                            <a href="#"
-                               class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 1-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 2-->
-                        <div class="small-box text-bg-success">
-                            <div class="inner">
-                                <h3>53<sup class="fs-5">%</sup></h3>
-                                <p>Bounce Rate</p>
+                            <div class="mb-3">
+                                <label for="last_name" class="form-label">{{ __('Apellidos') }}</label>
+                                <input type="text" name="last_name" class="form-control" id="last_name" aria-describedby="last_name" value="{{ $user->last_name }}">
                             </div>
-                            <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                 xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                            </svg>
-                            <a href="#"
-                               class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 2-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 3-->
-                        <div class="small-box text-bg-warning">
-                            <div class="inner">
-                                <h3>44</h3>
-                                <p>User Registrations</p>
+                            <div class="mb-3">
+                                <label for="departments" class="form-label">{{ __('Departamento') }}</label>
+                                <input type="text" name="departments" class="form-control" id="departments" aria-describedby="departments" value="{{ $user->departments }}">
                             </div>
-                            <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                 xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path
-                                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                            </svg>
-                            <a href="#"
-                               class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 3-->
-                    </div> <!--end::Col-->
-                    <div class="col-lg-3 col-6"> <!--begin::Small Box Widget 4-->
-                        <div class="small-box text-bg-danger">
-                            <div class="inner">
-                                <h3>65</h3>
-                                <p>Unique Visitors</p>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                                <input type="email" name="email" class="form-control" id="email" aria-describedby="email" value="{{ $user->email }}">
                             </div>
-                            <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                                 xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path clip-rule="evenodd" fill-rule="evenodd"
-                                      d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                                <path clip-rule="evenodd" fill-rule="evenodd"
-                                      d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                            </svg>
-                            <a href="#"
-                               class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                                More info <i class="bi bi-link-45deg"></i> </a>
-                        </div> <!--end::Small Box Widget 4-->
-                    </div> <!--end::Col-->
-                </div> <!--end::Row--> <!--begin::Row-->
+                        </div>
+
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-success">{{ __('Editar') }}</button>
+                        </div>
+                    </form>
+                </div>
 
             </div> <!--end::Container-->
         </div> <!--end::App Content-->
@@ -251,7 +211,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script>
 <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-<script src="../../dist/js/adminlte.js"></script> <!--end::Required Plugin(AdminLTE)-->
+<script src="../../../dist/js/adminlte.js"></script> <!--end::Required Plugin(AdminLTE)-->
 <!--begin::OverlayScrollbars Configure-->
 <script>
     const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
