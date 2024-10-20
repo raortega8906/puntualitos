c734ea9120f66103c66e680c2a2a834f3dfcbda0
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-20 20:29:27 +0200

    Modified dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,14 +102,6 @@
                                 {{ __('Registrar incidencia') }}
                             </a>
                         </div>
-                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path
-                                d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"></path>
-                        </svg>
-                        <a href="#"
-                           class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            <i class="bi bi-link-45deg"></i> </a>
                     </div> <!--end::Small Box Widget 1-->
                 </div> <!--end::Col-->
 
@@ -125,8 +117,8 @@
                                 <thead>
                                 <tr>
                                     <th class="text-bg-success text-white">{{ __('Día') }}</th>
-                                    <th class="text-bg-success text-white">{{ __('Checkin') }}</th>
-                                    <th class="text-bg-success text-white">{{ __('Checkout') }}</th>
+                                    <th class="text-bg-success text-white">{{ __('Entrada') }}</th>
+                                    <th class="text-bg-success text-white">{{ __('Salida') }}</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -138,11 +130,11 @@
                                     @if(auth()->user()->id == $attendance->user_id && $i++ < 10)
                                         <tr>
                                             <td>{{ Carbon::parse($attendance->created_at)->format('Y/m/d') }}</td>
-                                            <td>{{ Carbon::parse($attendance->check_in)->format('H:i:s') }}</td>
+                                            <td>{{ Carbon::parse($attendance->check_in)->format('H:i') }}</td>
                                             @if($attendance->check_out != null)
-                                                <td>{{ Carbon::parse($attendance->check_out)->format('H:i:s') }}</td>
+                                                <td>{{ Carbon::parse($attendance->check_out)->format('H:i') }}</td>
                                             @else
-                                                <td> -</td>
+                                                <td> - </td>
                                             @endif
                                         </tr>
                                     @endif
@@ -150,15 +142,7 @@
                                 </tbody>
                             </table>
                         </div>
-                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path
-                                d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
-                        </svg>
-                        <a href="#"
-                           class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
-                            <i class="bi bi-link-45deg"></i>
-                        </a>
+
                     </div> <!--end::Small Box Widget 2-->
                 </div> <!--end::Col-->
 
@@ -171,14 +155,7 @@
                             <p>{{ __('Departamento: '). Auth::user()->departments }}</p>
                             <p>{{ __('Email: '). Auth::user()->email }}</p>
                         </div>
-                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path
-                                d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                        </svg>
-                        <a href="#"
-                           class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
-                            <i class="bi bi-link-45deg"></i> </a>
+
                     </div> <!--end::Small Box Widget 3-->
 
                     <div class="small-box text-bg-warning">
@@ -188,14 +165,6 @@
                             <p>{{ __('Vacaciones usadas: '). 22 - Auth::user()->holidays }}</p>
                             <p>{{ __('Vacaciones restantes: '). Auth::user()->holidays }}</p>
                         </div>
-                        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path
-                                d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
-                        </svg>
-                        <a href="#"
-                           class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover">
-                            <i class="bi bi-link-45deg"></i> </a>
                     </div> <!--end::Small Box Widget 3-->
                 </div> <!--end::Col-->
 
