8985352f5cd4de1e53dc2172845b21524a41e141
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-21 00:30:52 +0200

    finalize welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,7 @@
             <h2 class="text-2xl font-bold mb-4">{{ __('Registro Fácil') }}</h2>
             <p class="mb-4">{{ __('Registra entradas y salidas con solo un clic. Nuestra interfaz intuitiva hace que el proceso
                 sea rápido y sin complicaciones.') }}</p>
-            <a href="{{ asset('videos/demo.webm') }}" class="btn-primary px-6 py-2 rounded-md inline-block">{{ __('Ver demo') }}</a>
+            <a href="{{ asset('videos/demo.webm') }}" target="_blank" class="btn-primary px-6 py-2 rounded-md inline-block">{{ __('Ver demo') }}</a>
         </div>
         <div class="card rounded-lg shadow-lg p-8">
             <h2 class="text-2xl font-bold mb-4">{{ __('Reportes Detallados') }}</h2>
