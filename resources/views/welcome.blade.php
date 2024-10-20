6569a07daf9d3f2079ec0587d72dd10c2c36cf25
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-20 23:58:42 +0200

    Add demo video and finalize welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -173,8 +173,8 @@
                           d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
                 <h3 class="text-xl font-semibold mb-2">{{ __('Horario de Soporte') }}</h3>
-                <p>Lunes a Viernes: 9:00 AM - 6:00 PM</p>
-                <p>Sábados, Domingos y festivos: Cerrado</p>
+                <p>Lunes a Viernes: 09:00 - 18:00</p>
+                <p>Sábados, Domingos y festivos: No operativo</p>
             </div>
             <div class="card rounded-lg shadow-lg p-6 text-center">
                 <svg class="w-12 h-12 mx-auto mb-4 text-blue-900" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -192,7 +192,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                 </svg>
-                <h3 class="text-xl font-semibold mb-2">{{ __('Recursos') }}</h3>
+                <h3 class="text-xl font-semibold mb-2">{{ __('Datos') }}</h3>
                 <p>Consulta nuestra documentación y guías de usuario obtener más información.</p>
             </div>
         </div>
