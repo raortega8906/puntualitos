09ea7580d4572f40566011ef93f8f61ce47ff118
Rafael A. Ortega <raortega8906@gmail.com>
2024-10-23 23:42:49 +0200

    enabling the ip with the api

--- resources/views/layouts/puntualitos.blade.php
+++ resources/views/layouts/puntualitos.blade.php
@@ -215,8 +215,8 @@
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        let userIp = '79.117.222.102';
-        // let userIp = '';
+        // let userIp = '79.117.222.102';
+        let userIp = '';
 
         // Obtener la IP pública del usuario usando la API de ipify
         fetch('https://api.ipify.org?format=json', { mode: 'no-cors' })
