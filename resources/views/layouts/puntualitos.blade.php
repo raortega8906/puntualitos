34bdba08da89ab458971e1c6c795d404a10db897
Rafael A. Ortega <raortega8906@gmail.com>
2024-11-16 23:20:28 +0100

    Add images in webp and add alt and width

--- resources/views/layouts/puntualitos.blade.php
+++ resources/views/layouts/puntualitos.blade.php
@@ -98,7 +98,7 @@
     </nav> <!--end::Header--> <!--begin::Sidebar-->
     <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
         <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="{{ route('welcome')}}" class="brand-link">
-                <!--begin::Brand Image--> <img src="{{ asset('/images/logo_bg_removed.png') }}" alt="Puntualitos Logo"
+                <!--begin::Brand Image--> <img src="{{ asset('/images/logo_bg_removed.webp') }}" alt="Puntualitos Logo"
                                                class="brand-image opacity-75 shadow"> <!--end::Brand Image-->
                 <!--begin::Brand Text--> <span class="brand-text fw-light">{{ __('Puntualitos') }}</span> <!--end::Brand Text-->
             </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
