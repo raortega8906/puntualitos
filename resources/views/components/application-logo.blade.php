9b5544281c150ba7ad64f6b72902d8c96f7cdf9e
Rafael A. Ortega <raortega8906@gmail.com>
2024-11-17 19:36:28 +0100

    Switch extension of logo to webp

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo_bg_removed.png') }}" alt="Puntualito Logo" class="w-20 h-20 fill-current text-gray-500">
+<img src="{{ asset('images/logo_bg_removed.webp') }}" alt="Puntualito Logo" class="w-20 h-20 fill-current text-gray-500">
